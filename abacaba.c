ee0c3125eb3162246ccb5f243b5bbb85b9e8f975
George <37293288+Jorjeous@users.noreply.github.com>
2018-09-23 14:11:30 +0300

    Update abacaba.c

--- abacaba.c
+++ abacaba.c
@@ -1,29 +1,20 @@
 #include <stdio.h>
-//#include <malloc.h>
-//#include <mem.h>
 #include <stdlib.h>
 #include <string.h>
-
-// все закомментированные и лишние пустые строки нужно удалять
 
 void GenStr(int n, char *string){
     char str[1];
     int i;
-
-
     if(n < 1)
     {
         return;
     }
-
     string[0] = 'a';
-    //printf("%s", string);
-
+    
     for(i = 1; i < n; i++){
         str[0] = (char) ('a' + i);
         strcat(string, str);
         strncpy(string + (1 << i), string, (1<< i) - 1);
-        //printf("||%s||\n", string);
     }
 }
 
